09de2e13021ca6ee7c1b038c4ba5e8f58ed8f8ae
monolok <antoinebertin@me.com>
2024-04-04 12:28:13 +0200

    lets go

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name='antoine',
     version='0.1',
-    packages=['antoine'],
+    packages=['antoine'],  # 'antoine' is the name of the package
     entry_points={
         'console_scripts': [
-            'my_package=my_package.__main__:main',
+            'antoine=antoine.app:main',  # 'antoine' is the name of the package and 'app.py' contains a function 'main'
         ],
     },
     include_package_data=True,
