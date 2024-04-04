0f8741352f58a577fde871eff491851c947a5d73
monolok <antoinebertin@me.com>
2024-04-04 15:36:30 +0200

    1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ print("Installing Antoine...")
 
 setup(
     name='antoine',
-    version='1.7',
+    version='1.8',
     packages=['antoine'],  # 'antoine' is the name of the package
     author='Antoine Bertin',
     author_email='monolok35@gmail.com',
