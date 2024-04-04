9e71f836e9a814e937dd88ef101f7ee966bacfca
monolok <antoinebertin@me.com>
2024-04-04 16:05:53 +0200

    2.0

---
+++ antoine/post_install.py
@@ -0,0 +1,8 @@
+# post_install.py
+
+def main():
+    print("Antoine is successfully installed!")
+    print("To hire Antoine, run 'hireantoine' in your terminal.")
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
 from setuptools import setup
-from setuptools.command.install import install
-
-class CustomInstallCommand(install):
-    def run(self):
-        print("Installing Antoine...")
-        install.run(self)
-        print("Antoine is successfully installed!")
-        print("To hire Antoine, run 'hire antoine now' in your terminal.")
 
 setup(
     name='antoine',
-    version='1.9',
+    version='2.0',
     packages=['antoine'],  # 'antoine' is the name of the package
     author='Antoine Bertin',
     author_email='monolok35@gmail.com',
@@ -19,12 +11,10 @@ setup(
     license='MIT',
     entry_points={
         'console_scripts': [
-            'hire antoine now=antoine.app:main',  # 'antoine' is the name of the package and 'app.py' contains a function 'main'
+            'hireantoine=antoine.app:main',  # 'antoine' is the name of the package and 'app.py' contains a function 'main'
         ],
     },
     include_package_data=True,
     install_requires=[],
-    cmdclass={
-        'install': CustomInstallCommand,
-    }
+    scripts=['post_install.py']
 )
