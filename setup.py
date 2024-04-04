796b42617bded9e30ff1b60860bdb96f6a15d9bf
monolok <antoinebertin@me.com>
2024-04-04 13:48:57 +0200

    antoine

--- setup.py
+++ setup.py
@@ -5,18 +5,15 @@ print("Installing Antoine...")
 
 setup(
     name='antoine',
-    version='0.2',
-    author='Antoine Bertin',
-    author_email='monolok35@gmail.com',
-    include_package_data=True,
-    install_requires=[],
-    url='https://github.com/monolok/antoine',
-    license='MIT',
+    version='1.1',
+    packages=['antoine'],  # 'antoine' is the name of the package
     entry_points={
         'console_scripts': [
-            'antoine=antoine.app:main',
+            'antoine=antoine.app:main',  # 'antoine' is the name of the package and 'app.py' contains a function 'main'
         ],
     },
+    include_package_data=True,
+    install_requires=[],
 )
 
 # Print a message after installation
