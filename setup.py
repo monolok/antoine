fb1d096c6124252d605f4b87b62b0e9bb0a4a75c
monolok <antoinebertin@me.com>
2024-04-04 15:16:19 +0200

    1.6

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ print("Installing Antoine...")
 
 setup(
     name='antoine',
-    version='1.4',
+    version='1.6',
     packages=['antoine'],  # 'antoine' is the name of the package
     entry_points={
         'console_scripts': [
-            'hireantoine=antoine.app:main',  # 'antoine' is the name of the package and 'app.py' contains a function 'main'
+            'hireantoinenow=antoine.app:main',  # 'antoine' is the name of the package and 'app.py' contains a function 'main'
         ],
     },
     include_package_data=True,
